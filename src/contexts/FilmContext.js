a6245511c5b2b6dd3aab36c8b6a58124b3e17b91
Aleksander <aleksandergorecki80@gmail.com>
2020-11-20 22:47:38 +0000

    Add local storag

--- src/contexts/FilmContext.js
+++ src/contexts/FilmContext.js
@@ -1,14 +1,18 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { filmReducer } from '../reducers/filmReducer';
 
 export const FilmContext = createContext();
 
 const FilmContextProvider = (props) => {
-    const [ films, dispatch ] = useReducer( filmReducer, []);
-    
-
-
+    const intValue = () => {
+        const localData = localStorage.getItem('films');
+        return localData ? JSON.parse(localData) : [];
+    }
+    const [ films, dispatch ] = useReducer( filmReducer, [], intValue);
+    useEffect(() => {
+        localStorage.setItem('films', JSON.stringify(films));
+    }, [films]);
     return ( 
         <FilmContext.Provider value={{films, dispatch}}>
             { props.children }
