efe4cddb83d3ce53c84f4f3a95f0124f9eee9388
Aleksander <aleksandergorecki80@gmail.com>
2021-01-14 23:52:09 +0000

    Add OMDb API request

--- src/components/SearchForFilmData.js
+++ src/components/SearchForFilmData.js
@@ -1,9 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+
 
 const SearchForFilmData = () => {
+    const [searchedPhrase, setSearchedPhrase] = useState('');
+
+    const onHandleChange = (event) => {
+        setSearchedPhrase(event.target.value);
+    }
+    const searchMovie = (event) => {
+        event.preventDefault();
+        console.log(searchedPhrase);
+        axios.get(`http://www.omdbapi.com/?s=${searchedPhrase}&apikey=${process.env.REACT_APP_API_KEY}`)
+        .then((res) => {
+            const movies = res.data.Search;
+            console.log(movies);
+            //  TU SKOŃCZYŁEM ZASTAMÓW SIĘ CO DALEJ
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }
+    
     return ( 
         <div>
-            Search ForFi lmData
+            Search in OMDb
+            <form onSubmit={searchMovie}>
+                <input type="text" placeholder="Search Movie" onChange={onHandleChange} />
+                <input type="submit" value="Search" />
+            </form>
         </div>
      );
 }
