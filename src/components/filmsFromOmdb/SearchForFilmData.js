f86f7c3901cda651073fd7f7a5d107a35d1ecdd2
Aleksander <aleksandergorecki80@gmail.com>
2021-01-15 23:15:14 +0000

    Add next and previous buttons

--- src/components/filmsFromOmdb/SearchForFilmData.js
+++ src/components/filmsFromOmdb/SearchForFilmData.js
@@ -4,50 +4,47 @@ import React, { useState, useEffect } from "react";
 
 
 const SearchForFilmData = () => {
+
+    const [tapedPhrase, setTaphedPhrase] = useState('');
     const [searchedPhrase, setSearchedPhrase] = useState('');
     const [moviesList, setMoviesList] = useState('');
     const [page, setPage] = useState(1);
- 
+
     const onHandleChange = (event) => {
-        setSearchedPhrase(event.target.value);
+        setTaphedPhrase(event.target.value);
     }
     const searchMovie = () => {
-    // const searchMovie = (event) => {
-        // event.preventDefault();
-        axios.get(`http://www.omdbapi.com/?s=${searchedPhrase}&apikey=${process.env.REACT_APP_API_KEY}&page=${page}`)
-        .then((res) => {
-            const movies = res.data.Search;
-            setMoviesList(movies);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+        setSearchedPhrase(tapedPhrase);
     }
-    const onNextPreviousPage = (value) =>{
-        console.log(page);
-        setPage(page+value);
-        console.log(page);
-        searchMovie();
-        console.log(page);
+    const onNextPreviousPage = (value) => {
+        setPage(page + value);
     }
 
-    useEffect(()=>{
-        console.log('useEfect', page)
-        // searchMovie();
-    }, [page]);
-
-    console.log(page, 'outside');
-    return ( 
+    useEffect(() => {
+        searchedPhrase &&
+            axios.get(`http://www.omdbapi.com/?s=${searchedPhrase}&apikey=${process.env.REACT_APP_API_KEY}&page=${page}`)
+                .then((res) => {
+                    const movies = res.data.Search;
+                    setMoviesList(movies);
+                    console.log('get');
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+    }, [searchedPhrase, page]);
+console.log(moviesList);
+    return (
         <div>
             <p>Search in OMDb</p>
+            <form onSubmit={searchMovie}>
                 <input type="text" placeholder="Search Movie" onChange={onHandleChange} />
-                <input type="submit" value="Search" onClick={searchMovie}/>
-                <FilmsList moviesList={moviesList}/>
-
-                {/* {page>1 && <button onClick={()=>{setPage(page-1)}}>Previous</button>} */}
-                {moviesList.length >=10 && <button onClick={()=>{onNextPreviousPage(1)}}>Next</button>}
+                <input type="submit" value="Search" />
+                {moviesList ? <FilmsList moviesList={moviesList} /> : 'nothing to display'}
+            </form>
+            {page>1 && <button onClick={()=>{onNextPreviousPage(-1)}}>Previous</button>}
+            {moviesList.length >=10 && <button onClick={()=>{onNextPreviousPage(1)}}>Next</button>}
         </div>
-     );
+    );
 }
- 
+
 export default SearchForFilmData;
