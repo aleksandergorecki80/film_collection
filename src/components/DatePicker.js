8750bd648029a234c5d039e332d9ee7d1cfe2170
Aleksander <aleksandergorecki80@gmail.com>
2021-03-24 23:14:07 +0000

    Implementing DatePicker

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,59 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { getYear, eachYearOfInterval } from 'date-fns';
 
-const DatePicker = ({setPickedYear}) => {
-  const [ start, setStart ] = useState(0);
-  const [ breakpoint, setBreakpoint ] = useState(25);
-  const [ pickerDatesArray, setPickerDatesArray ] = useState('');
-  const [ arraySlice, setArraySlice ] = useState('');
-  const [ pickedDate, setPickedDate ] = useState('');
-  const [ numberOfSlices, setNumberOfSlices ] = useState('');
-  const [ currentSlice, setCurrentSlice ] = useState(1);
-  const [ openPicker, setOpenPicker ] = useState(false);
-  
- const onHandleYearPicker = () => {
-  const interval = {
-      start: new Date('1895'),
-      end: new Date(),
-    };
-    const datesArray = eachYearOfInterval(interval);
-  setPickerDatesArray(datesArray.reverse());
-  setNumberOfSlices(Math.ceil(datesArray.length/25));
-  setOpenPicker(true);
- }
- const pickDateHandler = (event) => {
-  setPickedDate(event.target.innerText);
-  setOpenPicker(false);
- }
- const nextHandler = () => {
-  setStart(start+25);
-  setBreakpoint(breakpoint+25);
-  setArraySlice(pickerDatesArray.slice(start, breakpoint));
-  setCurrentSlice(currentSlice+1);
- };
- const previousHandler = () => {
-  setStart(start-25);
-  setBreakpoint(breakpoint-25);
-  setArraySlice(pickerDatesArray.slice(start, breakpoint));
-  setCurrentSlice(currentSlice-1);
- };
- useEffect(() => {
-  setArraySlice(pickerDatesArray.slice(start, breakpoint));
- }, [ pickerDatesArray, breakpoint, start ]);
- useEffect(() => {
-  setPickedYear(pickedDate);
- }, [ pickedDate, setPickedYear ]);
 
- return (
-    <div className="year-picker">
+
+const DatePicker = ({defaultYear}) => {
+    // const [ start, setStart ] = useState(0);
+    // const [ breakpoint, setBreakpoint ] = useState(25);
+    const [ pickerDatesArray, setPickerDatesArray ] = useState('');
+    // const [ arraySlice, setArraySlice ] = useState('');
+    // const [ pickedDate, setPickedDate ] = useState('');
+    // const [ numberOfSlices, setNumberOfSlices ] = useState('');
+    // const [ currentSlice, setCurrentSlice ] = useState(1);
+    // const [ openPicker, setOpenPicker ] = useState(false);
+    
+   const onHandleYearPicker = () => {
+    const interval = {
+        start: new Date('1895'),
+        end: new Date(),
+      };
+    const datesArray = eachYearOfInterval(interval);
+    setPickerDatesArray(datesArray.reverse());
+    // setNumberOfSlices(Math.ceil(datesArray.length/25));
+    // setOpenPicker(true);
+   }
+//    const pickDateHandler = (event) => {
+//     setPickedDate(event.target.innerText);
+//     setOpenPicker(false);
+//    }
+//    const nextHandler = () => {
+//     setStart(start+25);
+//     setBreakpoint(breakpoint+25);
+//     setArraySlice(pickerDatesArray.slice(start, breakpoint));
+//     setCurrentSlice(currentSlice+1);
+//    };
+//    const previousHandler = () => {
+//     setStart(start-25);
+//     setBreakpoint(breakpoint-25);
+//     setArraySlice(pickerDatesArray.slice(start, breakpoint));
+//     setCurrentSlice(currentSlice-1);
+//    };
+//    useEffect(() => {
+//     setArraySlice(pickerDatesArray.slice(start, breakpoint));
+//    }, [ pickerDatesArray, breakpoint, start ]);
+//    useEffect(() => {
+//     setPickedYear(pickedDate);
+//    }, [ pickedDate, setPickedYear ]);
+    return ( 
+<div className="year-picker">
       <input
         type="text"
         placeholder="Year"
-        defaultValue={pickedDate}
+        defaultValue={defaultYear}
         name="year"
         onFocus={onHandleYearPicker}
       />
-      <div className="year-picker-container">
+      {/* <div className="year-picker-container">
      {openPicker && <div 
           className={"previous " + 
           (currentSlice === 1 ? "hidden" : '') 
@@ -73,8 +74,9 @@ const DatePicker = ({setPickedYear}) => {
         {openPicker && <div 
         className={ "next " + (currentSlice===numberOfSlices ? "hidden" : '') }
         onClick={nextHandler}></div>}
-      </div>
+      </div> */}
     </div>
-  );
-};
+    );
+}
+ 
 export default DatePicker;
--- src/components/FilmForm.js
+++ src/components/FilmForm.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addFilm } from '../actions/filmActions';
 import { editFilm } from '../actions/filmActions';
+import DatePicker from './DatePicker';
 
 class FilmForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       posterFile: '',
-      // pickerDatesArray: '',
       film: {
         title: this.props.film ? this.props.film.title : '',
         format: this.props.film ? this.props.film.format : 'unknown',
@@ -19,140 +19,170 @@ class FilmForm extends React.Component {
     };
   }
   handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     if (this.props.match.params.id) {
       this.props.editFilm(this.state.film, this.props.match.params.id);
-      this.props.history.push("/");
+      this.props.history.push('/');
     } else {
       this.props.addFilm(this.state.film);
-      this.props.history.push("/");
+      this.props.history.push('/');
     }
-    
   };
 
   onChange = (event) => {
     this.setState({
       film: {
         ...this.state.film,
-        [event.target.name]: event.target.value
-      }
-    })
-  }
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  setPickedYear = (year) => {
+    this.setState({
+      film: {
+        ...this.state.film,
+        year,
+      },
+    });
+  };
 
   onHanleFile = (event) => {
     this.setState({
       film: {
         ...this.state.film,
-      posterFile: event.target.files[0],
-      posterName: event.target.files[0].name
-    }
-    })
-  }
+        posterFile: event.target.files[0],
+        posterName: event.target.files[0].name,
+      },
+    });
+  };
 
   componentDidMount() {
     // EDITION OF EGZISTING FILM
-    if(this.props.match.params.id){
+    if (this.props.match.params.id) {
       const film = this.props.films.find((film) => {
         return film._id === this.props.match.params.id;
       });
-      film ? 
-        this.setState({
-          film: {
-            title: film.title,
-            format: film.format,
-            condition: film.condition,
-            year: film.year,
-            posterName: film.posterName
-          }
-        })
-      : this.props.history.push("/");
+      film
+        ? this.setState({
+            film: {
+              title: film.title,
+              format: film.format,
+              condition: film.condition,
+              year: film.year,
+              posterName: film.posterName,
+            },
+          })
+        : this.props.history.push('/');
     }
     // CONFIRMING DATA OF FILM IMPORTED FROM OMDB API
-    if(this.props.match.path === '/confirm_data'){
+    if (this.props.match.path === '/confirm_data') {
+      console.log(this.props.importedData);
       this.setState({
         film: {
           ...this.state.film,
           title: this.props.importedData.title,
           year: this.props.importedData.year,
-          posterName: this.props.importedData.posterName
-        }
-      })
+          posterName: this.props.importedData.posterName,
+        },
+      });
     }
   }
   render() {
+    console.log(this.state, 'state');
     return (
-      <form onSubmit={this.handleSubmit} className="content">
-        <input type="text" placeholder="Film title"
-          value={this.state.film.title}
-          onChange={this.onChange} 
-          name="title" 
-          required 
-        />
-        <input type="text" placeholder="Year"
+      <div>
+        <form onSubmit={this.handleSubmit} className="content">
+          <input
+            type="text"
+            placeholder="Film title"
+            value={this.state.film.title}
+            onChange={this.onChange}
+            name="title"
+            required
+          />
+          {/* <input type="text" placeholder="Year"
           value={this.state.film.year}
           onChange={this.onChange} 
           name="year"  
-        />
-        <select value={this.state.film.format} onChange={this.onChange} name="format">
-          <option value="unknown" disabled required> -- select a format -- </option>
-          <option value="DVD">DVD</option>
-          <option value="BluRey">BluRey</option>
-        </select>
-        <div className="radio">
-          <label>
-            <input
-              name="condition"
-              type="radio"
-              value="New"
-              checked={this.state.film.condition === "New"}
-              onChange={this.onChange}
-            />
-            New
-          </label>
-        </div>
-        <div className="radio">
-          <label>
-            <input
-              name="condition"
-              type="radio"
-              value="Used"
-              checked={this.state.film.condition === "Used"}
-              onChange={this.onChange}
-            />
-            Used
-          </label>
-        </div>
-        <div>
-          {this.state.film.posterName &&
-          !this.state.film.posterName.startsWith('https://') ? (
-            <img src={`/uploads/${this.state.film.posterName}`} alt="cover" />
+        /> */}
+
+          <DatePicker setPickedYear={this.setPickedYear} defaultYear={this.state.film.year}/>
+
+          <select
+            value={this.state.film.format}
+            onChange={this.onChange}
+            name="format"
+          >
+            <option value="unknown" disabled required>
+              {' '}
+              -- select a format --{' '}
+            </option>
+            <option value="DVD">DVD</option>
+            <option value="BluRey">BluRey</option>
+          </select>
+          <div className="radio">
+            <label>
+              <input
+                name="condition"
+                type="radio"
+                value="New"
+                checked={this.state.film.condition === 'New'}
+                onChange={this.onChange}
+              />
+              New
+            </label>
+          </div>
+          <div className="radio">
+            <label>
+              <input
+                name="condition"
+                type="radio"
+                value="Used"
+                checked={this.state.film.condition === 'Used'}
+                onChange={this.onChange}
+              />
+              Used
+            </label>
+          </div>
+          <div>
+            {this.state.film.posterName &&
+            !this.state.film.posterName.startsWith('https://') ? (
+              <img src={`/uploads/${this.state.film.posterName}`} alt="cover" />
+            ) : (
+              <img src={this.state.film.posterName} alt="cover" />
+            )}
+            <input type="file" onChange={this.onHanleFile} />
+            <label></label>
+          </div>
+          {!this.props.match.params.id ? (
+            <input type="submit" value="Add film" className="btn btn-add" />
           ) : (
-            <img src={this.state.film.posterName} alt="cover" />
+            <input type="submit" value="Update film" className="btn btn-add" />
           )}
-          <input type="file" onChange={this.onHanleFile} />
-          <label></label>
-        </div>
-        {!this.props.match.params.id ? (
-          <input type="submit" value="Add film" className="btn btn-add" />
-        ) : (
-          <input type="submit" value="Update film" className="btn btn-add" />
-        )}
-      </form>
+        </form>
+        
+        {/* <DatePicker setPickedYear={this.setPickedYear} /> */}
+      </div>
     );
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    addFilm: (film, formData) => { dispatch(addFilm(film, formData)) },
-    editFilm: (film, id) => { dispatch(editFilm(film, id)) }
-  }
+    addFilm: (film, formData) => {
+      dispatch(addFilm(film, formData));
+    },
+    editFilm: (film, id) => {
+      dispatch(editFilm(film, id));
+    },
+  };
 };
 
 const mapStateToProps = (state) => {
   return {
     films: state.films,
-    importedData: state.importedData
-  }
+    importedData: state.importedData,
+  };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(FilmForm);
